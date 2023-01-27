c87b45dfabff35f3f146b3313ed4bd479ab23443
Akshay Mule <89520508+Akshay-mule@users.noreply.github.com>
2023-01-27 22:17:18 +0530

    Add files via upload

---
+++ TodoList/script.js
@@ -0,0 +1,55 @@
+const todoForm = document.querySelector(".form-todo");
+const todoInput = document.querySelector(".form-todo input[type='text']");
+const todoList = document.querySelector(".todo-list");
+
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();                  // to refresh a page to see the result.
+    const newTodoText = todoInput.value; // to add the list of element
+    const newLi = document.createElement("li");
+    const newLiInnerHtml = `
+        <span class="text">${newTodoText}</span>
+        <div class="todo-buttons">
+            <button class="todo-btn done">Done</button>
+            <button class="todo-btn remove">Remove</button>
+        </div>`;
+    newLi.innerHTML = newLiInnerHtml;
+    todoList.append(newLi);
+    todoInput.value = "";            // after adding  remove element from inputbox. to add another i/p element
+});
+
+
+
+todoList.addEventListener("click", (e) => {
+    if (e.target.classList.contains("remove")) {
+        const targetedLi = e.target.parentNode.parentNode;
+        targetedLi.remove();
+    }
+    if (e.target.classList.contains("done")) {
+        const liSpan = e.target.parentNode.previousElementSibling;
+        liSpan.style.textDecoration = "line-through";
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
